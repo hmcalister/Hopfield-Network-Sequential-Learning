cfa16aa7004921fc670e67bfb3bd946ee33b38c4
Hayden McAlister <haydenmcalister49@gmail.com>
2022-05-04 03:36:02 +1200

    Update main method to use new methods and classes

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import HopfieldNetwork
 import PatternManager
+from HopfieldUtils import *
 import numpy as np
 import matplotlib.pyplot as plt
 
 np.set_printoptions(precision=2)
-
 N = 1000
-energyFunction = HopfieldNetwork.EnergyFunction.BipolarEnergyFunction()
-activationFunction = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
-updateRule = HopfieldNetwork.UpdateRule.AsynchronousList(activationFunction)
-learningRule = HopfieldNetwork.LearningRule.Hebbian()
-
-BipolarHeaviside = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
-
-network = HopfieldNetwork.BipolarHopfieldNetwork(
-    N=N,
-    energyFunction=energyFunction,
-    activationFunction=activationFunction,
-    updateRule=updateRule,
-    learningRule=learningRule
-)
-
-mappingFunction = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
+network = HopfieldNetwork.BinaryHopfieldNetwork(N)
+mappingFunction = HopfieldNetwork.UpdateRule.ActivationFunction.BinaryHeaviside()
 patternManager = PatternManager.SequentialLearningPatternManager(N, mappingFunction)
 
+# energyFunction = HopfieldNetwork.EnergyFunction.BipolarEnergyFunction()
+# activationFunction = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
+# updateRule = HopfieldNetwork.UpdateRule.AsynchronousList(activationFunction)
+# learningRule = HopfieldNetwork.LearningRule.Hebbian()
+
+# BipolarHeaviside = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
+
+# network = HopfieldNetwork.GeneralHopfieldNetwork(
+#     N=N,
+#     energyFunction=energyFunction,
+#     activationFunction=activationFunction,
+#     updateRule=updateRule,
+#     learningRule=learningRule
+# )
+
+
 tasks = patternManager.createTasks(
-    numTasks=3,
-    numPatternsPerTask=33,
+    numTasks=10,
+    numPatternsPerTask=10,
     numNearbyMappingsPerPattern=0
 )
 
 seenPatterns = []
-taskAccuraciesOverEpochs = None
+taskPatternStabilities = None
 numStableOverEpochs = []
 
 print(network)
@@ -46,36 +48,15 @@ for task in tasks:
     # print(network.getWeights())
 
     acc,numStable=network.measureTaskPatternStability([task.taskPatterns for task in patternManager.taskPatternManagers])
-    if taskAccuraciesOverEpochs is None:
-        taskAccuraciesOverEpochs = acc.copy()
+    if taskPatternStabilities is None:
+        taskPatternStabilities = acc.copy()
     else:
-        taskAccuraciesOverEpochs = np.vstack([taskAccuraciesOverEpochs, acc.copy()]) 
+        taskPatternStabilities = np.vstack([taskPatternStabilities, acc.copy()]) 
     print(f"{acc=}")
 
-    numStable = network.measureNumStablePatterns(patternManager.allTaskPatterns)
     numStableOverEpochs.append(numStable)
     print(f"{numStable=}")
-
-
     print()
 
-xRange = np.arange(len(taskAccuraciesOverEpochs))
-for i in range(len(taskAccuraciesOverEpochs)):
-    plt.plot(xRange[i:], taskAccuraciesOverEpochs[i:, i])
-plt.ylim(-0.05,1.05)
-plt.legend([str(task) for task in tasks], bbox_to_anchor=(1.04, 0.5), loc='center left')
-plt.title("Accuracies by Task")
-plt.xlabel("Task Number")
-plt.ylabel("Task Accuracy")
-plt.tight_layout()
-plt.show()
-
-plt.plot(numStableOverEpochs)
-plt.axhline(0.14*N, color='r', linestyle='--', label="Hebbian Max")
-plt.axhline(max(numStableOverEpochs), color='b', linestyle='--', label="Actual Max")
-plt.legend(bbox_to_anchor=(1.04, 0.5), loc='center left')
-plt.title("Total stable patterns")
-plt.xlabel("Task Number")
-plt.ylabel("Total stable patterns")
-plt.tight_layout()
-plt.show()
+plotTaskPatternStability(taskPatternStabilities, fileName="graphs/MappedBinaryTaskPatternStability.png")
+plotTotalStablePatterns(numStableOverEpochs, N, fileName="graphs/MappedBinaryTotalStablePatterns.png")
