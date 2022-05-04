b17bada24cad782036c93a22f37ff175bb2059df
Hayden McAlister <haydenmcalister49@gmail.com>
2022-05-04 23:28:49 +1200

    Update main to test new functionality

--- main.py
+++ main.py
@@ -6,28 +6,26 @@ import matplotlib.pyplot as plt
 
 np.set_printoptions(precision=2)
 N = 1000
-network = HopfieldNetwork.BinaryHopfieldNetwork(N)
-mappingFunction = HopfieldNetwork.UpdateRule.ActivationFunction.BinaryHeaviside()
+
+mappingFunction = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
 patternManager = PatternManager.SequentialLearningPatternManager(N, mappingFunction)
 
-# energyFunction = HopfieldNetwork.EnergyFunction.BipolarEnergyFunction()
-# activationFunction = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
-# updateRule = HopfieldNetwork.UpdateRule.AsynchronousList(activationFunction)
-# learningRule = HopfieldNetwork.LearningRule.Hebbian()
+energyFunction = HopfieldNetwork.EnergyFunction.BipolarEnergyFunction()
+activationFunction = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
+updateRule = HopfieldNetwork.UpdateRule.AsynchronousList(activationFunction)
+learningRule = HopfieldNetwork.LearningRule.Delta(epochs=10)
 
-# BipolarHeaviside = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
-
-# network = HopfieldNetwork.GeneralHopfieldNetwork(
-#     N=N,
-#     energyFunction=energyFunction,
-#     activationFunction=activationFunction,
-#     updateRule=updateRule,
-#     learningRule=learningRule
-# )
+network = HopfieldNetwork.GeneralHopfieldNetwork(
+    N=N,
+    energyFunction=energyFunction,
+    activationFunction=activationFunction,
+    updateRule=updateRule,
+    learningRule=learningRule
+)
 
 
 tasks = patternManager.createTasks(
-    numTasks=10,
+    numTasks=3,
     numPatternsPerTask=10,
     numNearbyMappingsPerPattern=0
 )
@@ -40,23 +38,26 @@ print(network)
 for task in tasks:
     print(f"{task}")
     seenPatterns.extend(task.getTaskPatterns())
+    # print(f"Task Patterns: {task.taskPatterns}")
     # print(f"{seenPatterns=}")
 
     # randomMappings = patternManager.createRandomMappings(100, seenPatterns, changeRatio=0.05)
 
-    network.learnPatterns(task.taskPatterns)
-    # print(network.getWeights())
+    accuracies, numStable = network.learnPatterns(task.taskPatterns, [task.taskPatterns for task in patternManager.taskPatternManagers])
+    print(network.getWeights())
 
-    acc,numStable=network.measureTaskPatternStability([task.taskPatterns for task in patternManager.taskPatternManagers])
+    # acc,numStable=network.measureTaskPatternStability([task.taskPatterns for task in patternManager.taskPatternManagers])
     if taskPatternStabilities is None:
-        taskPatternStabilities = acc.copy()
+        taskPatternStabilities = np.array(accuracies).copy()
     else:
-        taskPatternStabilities = np.vstack([taskPatternStabilities, acc.copy()]) 
-    print(f"{acc=}")
+        taskPatternStabilities = np.vstack([taskPatternStabilities, accuracies.copy()])
+    print(f"Most Recent Epoch Accuracy: {accuracies[-1]}")
 
-    numStableOverEpochs.append(numStable)
-    print(f"{numStable=}")
+    numStableOverEpochs.extend(numStable)
+    print(f"Most Recent Epoch Stable States: {numStable[-1]}")
     print()
 
-plotTaskPatternStability(taskPatternStabilities, fileName="graphs/MappedBinaryTaskPatternStability.png")
-plotTotalStablePatterns(numStableOverEpochs, N, fileName="graphs/MappedBinaryTotalStablePatterns.png")
+plotTaskPatternStability(taskPatternStabilities, title="1000 Unit Bipolar Hopfield - Pattern Stability by Task",
+    legend=[str(task) for task in tasks])
+
+plotTotalStablePatterns(numStableOverEpochs, N, title="1000 Unit Bipolar Hopfield - Total Stable Patterns")
