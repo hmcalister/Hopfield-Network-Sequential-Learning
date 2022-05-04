0c2500e88e178a1f6170a0bad91649e608ec9103
Hayden McAlister <haydenmcalister49@gmail.com>
2022-05-04 23:26:55 +1200

    Better describe behavior in docstring

--- HopfieldNetwork/UpdateRule/ActivationFunction/BinaryHeaviside.py
+++ HopfieldNetwork/UpdateRule/ActivationFunction/BinaryHeaviside.py
@@ -6,14 +6,15 @@ class BinaryHeaviside(AbstractActivationFunction):
     def __init__(self):
         """
         Return a new instance of a BinaryHeaviside activation function
-        If input is zero, return 0
+        If input is 0.5, return 0
+        Maps (-inf, 0.5] to 0, (0.5,inf) to 1
         Output {0, 1}
         """
         pass
 
     def __call__(self, state:ndarray)->ndarray:
         
-        out=array(heaviside(state,0))
+        out=array(heaviside(state-0.5,0))
         return out
 
     def __str__(self):
