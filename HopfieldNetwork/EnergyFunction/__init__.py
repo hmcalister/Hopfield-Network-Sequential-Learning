8827f237ce68dd1f16dfc996c905eaada8fcc228
Hayden McAlister <haydenmcalister49@gmail.com>
2022-05-12 19:10:32 +1200

    Add MappedBinary energy function

--- HopfieldNetwork/EnergyFunction/__init__.py
+++ HopfieldNetwork/EnergyFunction/__init__.py
@@ -1,3 +1,4 @@
 from .AbstractEnergyFunction import AbstractEnergyFunction
 from .BipolarEnergyFunction import BipolarEnergyFunction
 from .BinaryEnergyFunction import BinaryEnergyFunction
+from .MappedBinaryEnergyFunction import MappedBinaryEnergyFunction
