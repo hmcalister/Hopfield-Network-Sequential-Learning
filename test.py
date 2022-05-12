b72b8440fcfa967fc322131c32e2044da77713b5
Hayden McAlister <haydenmcalister49@gmail.com>
2022-05-12 19:11:51 +1200

    Test method to read data form JSON files, plot multiple experiments on one graph

--- test.py
+++ test.py
@@ -1,27 +1,48 @@
-from typing import List
+from cProfile import label
+import os
+import json
+import shutil
 import numpy as np
-import PatternManager
-import HopfieldNetwork
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(12, 6))
+
+dataFiles = [
+    "1000Bipolar-Delta-100 Epochs-NoneInput0HeteroassociativeNoise-0AllowableError.json",
+    # "1000Bipolar-Delta-100 Epochs-AbsoluteInput0HeteroassociativeNoise-0AllowableError.json",
+    # "1000Bipolar-Delta-100 Epochs-NoneInput0.05HeteroassociativeNoise-0.001AllowableError.json",
+    "1000Bipolar-Delta-100 Epochs-NoneInput0.05HeteroassociativeNoise-0.01AllowableError.json",
+    "1000Bipolar-Delta-100 Epochs-AbsoluteInput0.05HeteroassociativeNoise-0.01AllowableError.json",
+    # "1000Bipolar-Delta-100 Epochs-AbsoluteInput0.05HeteroassociativeNoise-0AllowableError.json",
+    # "1000Bipolar-Delta-100 Epochs-RelativeInput0HeteroassociativeNoise-0AllowableError.json",
+    # "1000Bipolar-Delta-100 Epochs-NoneNoise-0.01AllowableError.json",
+    # "1000Bipolar-Delta-100 Epochs-NoneNoise-0.001AllowableError.json",
+
+    # "1000Bipolar-Hebbian-NoneNoise-0AllowableError.json",
+    # "1000Bipolar-Hebbian-NoneNoise-0.001AllowableError.json",
+    # "1000Bipolar-Hebbian-NoneNoise-0.005AllowableError.json",
+    # "1000Bipolar-Hebbian-NoneNoise-0.01AllowableError.json",
+]
 
 
-mappingFunction = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
-patternManager = PatternManager.SequentialLearningPatternManager(15, mappingFunction)
+for dataFile in dataFiles:
+    dataFile = f"data/{dataFile}"
+    print(dataFile)
+    shutil.copyfile(dataFile, f"data/data.json")
+    try:
+        with open(f"data/data.json", "rb") as f:
+            data = json.load(f)
+            plt.plot(np.asarray(data["numStableOverEpochs"]), 
+                label=f"{data['trainingInformation']['inputNoise']}, {data['trainingInformation']['heteroassociativeNoiseRatio']}, {data['networkDescription']['Allowable Learning State Error']}")
+    except Exception as e:
+        print(type(e))
+        print(e)
+        pass
 
-tasks:List[PatternManager.TaskPatternManager] = patternManager.createTasks(
-    numTasks=3,
-    numPatternsPerTask=5,
-    numNearbyMappingsPerPattern=5
-)
 
-for task in tasks:
-    print(task)
-    for taskPattern in task.getTaskPatterns():
-        print(taskPattern)
-    for nearbyMapping in task.getNearbyMappings():
-        print(f"{nearbyMapping[0]} -> \n{nearbyMapping[1]} : {patternManager.patternDistanceFunction(nearbyMapping[0], nearbyMapping[1])}")
-    print(len(task.getNearbyMappings()))
-
-randomMappings = patternManager.createRandomMappings(500, patternManager.allTaskPatterns)
-for randomMapping in randomMappings:
-    print(f"{randomMapping[0]} -> \n{randomMapping[1]} : {patternManager.patternDistanceFunction(randomMapping[0], randomMapping[1])}")
-print(len(randomMappings))
+plt.legend(bbox_to_anchor=(1.04, 0.5), loc='center left', title="Input, Heteroassociative, State Error")
+plt.title("1000 Unit Bipolar Hopfield Network - Total Stable Patterns\n 3 Tasks, 100 Patterns/Task\n Noise Experiments")
+plt.xlabel("Epoch")
+plt.ylabel("Total stable learned patterns")
+plt.tight_layout()
+plt.show()
