e0ec7ff3f28c67f6513c821cb6fdf46808f29d95
Hayden McAlister <haydenmcalister49@gmail.com>
2022-05-12 19:12:11 +1200

    Big changes to main to reflect small changes to EVERYTHING

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from turtle import update
 import HopfieldNetwork
+from HopfieldNetwork import LearningRule
+from HopfieldNetwork import UpdateRule
 import PatternManager
 from HopfieldUtils import *
 import numpy as np
-import matplotlib.pyplot as plt
 
 np.set_printoptions(precision=2)
 N = 1000
@@ -10,31 +12,37 @@ N = 1000
 mappingFunction = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
 patternManager = PatternManager.SequentialLearningPatternManager(N, mappingFunction)
 
+
 energyFunction = HopfieldNetwork.EnergyFunction.BipolarEnergyFunction()
 activationFunction = HopfieldNetwork.UpdateRule.ActivationFunction.BipolarHeaviside()
-updateRule = HopfieldNetwork.UpdateRule.AsynchronousList(activationFunction)
-learningRule = HopfieldNetwork.LearningRule.Delta(epochs=10)
+updateRule = HopfieldNetwork.UpdateRule.AsynchronousPermutation(activationFunction, energyFunction)
+learningRule = HopfieldNetwork.LearningRule.Delta(epochs=100)
+allowableLearningStateError = 0
+
+inputNoise = "Absolute"
+heteroassociativeNoiseRatio = 0.05
 
 network = HopfieldNetwork.GeneralHopfieldNetwork(
     N=N,
     energyFunction=energyFunction,
     activationFunction=activationFunction,
     updateRule=updateRule,
-    learningRule=learningRule
+    learningRule=learningRule,
+    allowableLearningStateError=allowableLearningStateError
 )
-
 
 tasks = patternManager.createTasks(
     numTasks=3,
-    numPatternsPerTask=10,
-    numNearbyMappingsPerPattern=0
+    numPatternsPerTask=100
 )
 
 seenPatterns = []
 taskPatternStabilities = None
 numStableOverEpochs = []
 
-print(network)
+print(network.getNetworkDescriptionString())
+print()
+
 for task in tasks:
     print(f"{task}")
     seenPatterns.extend(task.getTaskPatterns())
@@ -43,10 +51,10 @@ for task in tasks:
 
     # randomMappings = patternManager.createRandomMappings(100, seenPatterns, changeRatio=0.05)
 
-    accuracies, numStable = network.learnPatterns(task.taskPatterns, [task.taskPatterns for task in patternManager.taskPatternManagers])
-    print(network.getWeights())
+    accuracies, numStable = network.learnPatterns(task.taskPatterns, [task.taskPatterns for task in patternManager.taskPatternManagers], 
+        heteroassociativeNoiseRatio=heteroassociativeNoiseRatio, inputNoise=inputNoise)
+    # print(network.getWeights())
 
-    # acc,numStable=network.measureTaskPatternStability([task.taskPatterns for task in patternManager.taskPatternManagers])
     if taskPatternStabilities is None:
         taskPatternStabilities = np.array(accuracies).copy()
     else:
@@ -57,7 +65,29 @@ for task in tasks:
     print(f"Most Recent Epoch Stable States: {numStable[-1]}")
     print()
 
-plotTaskPatternStability(taskPatternStabilities, title="1000 Unit Bipolar Hopfield - Pattern Stability by Task",
-    legend=[str(task) for task in tasks])
+titleBasis = f"Bipolar {network.N} Unit - {network.learningRule}\n {inputNoise} Input, {heteroassociativeNoiseRatio} Heteroassociative Noise, {network.allowableLearningStateError} Allowable Stability Error"
+fileNameBasis = f"{network.N}Bipolar-{network.learningRule}-{inputNoise}Input{heteroassociativeNoiseRatio}HeteroassociativeNoise-{network.allowableLearningStateError}AllowableError"
 
-plotTotalStablePatterns(numStableOverEpochs, N, title="1000 Unit Bipolar Hopfield - Total Stable Patterns")
+plotTaskPatternStability(taskPatternStabilities, taskEpochBoundaries=[network.learningRule.epochs*i for i in range(len(tasks))], 
+    title=f"{titleBasis}\n Stability by Task",
+    legend=[str(task) for task in tasks], figsize=(12,6),
+    fileName=f"graphs/{fileNameBasis}-StabilityByTask.png"
+    )
+
+plotTotalStablePatterns(numStableOverEpochs, N, hebbianMaximumCapacity=network.getHebbianMaxRatio(),
+    title=f"{titleBasis}\n Total Stable Patterns", 
+    figsize=(12,6),
+    fileName=f"graphs/{fileNameBasis}-TotalStablePatterns.png"
+    )
+
+saveDataAsJSON(f"data/{fileNameBasis}.json", 
+    networkDescription = network.getNetworkDescriptionJSON(),
+    trainingInformation= {
+        "inputNoise": inputNoise,
+        "heteroassociativeNoiseRatio": heteroassociativeNoiseRatio
+    },
+    taskPatternStabilities = taskPatternStabilities.tolist(),
+    taskEpochBoundaries = [network.learningRule.epochs*i for i in range(len(tasks))],
+    numStableOverEpochs = numStableOverEpochs,
+    weights=network.weights.tolist(),
+    tasks=[np.array(task.taskPatterns).tolist() for task in patternManager.taskPatternManagers])
